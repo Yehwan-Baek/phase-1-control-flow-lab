c0bfcfa7c3e0f85c1ad8a4f6422425e5abbbdf16
Yehwan Baek <baekokok98@gmail.com>
2023-03-08 15:57:48 +1100

    added code

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    console.log("This one is on me!")
+  } else if(feet <= 2000){
+    console.log("That will be twenty bucks.")
+  } else if(2000 < feet && feet <= 2500){
+    console.log("I will gladly take your thirty bucks.")
+  } else {
+    console.log("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  city === NYC ? console.log("ok, sounds good") : console.log("No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+  case 'generous' : console.log("Thank you so much.") ;
+  case 'not as generous' : console.log("Thank you.") ;
+  break;
+  default : console.log("Bye.") ;
+  }
 }
